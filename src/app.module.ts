c47947c6dfb8cd13007d1ef7e0d34d6b220a6599
Willian Louback <williandlouback@gmail.com>
2024-09-18 01:49:45 -0300

    redundancy deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/users/users.module';
-// import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
   imports: [UserModule],
